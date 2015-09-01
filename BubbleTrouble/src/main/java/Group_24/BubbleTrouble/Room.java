a1991ba0f5cc0147e9537e9229073bc37bb7eed4
Chris Berg <c.c.berg@student.tudelft.nl>
2015-09-01 17:14:26 +0200

    basic Fysics and Collision detection added

--- BubbleTrouble/src/main/java/Group_24/BubbleTrouble/Room.java
+++ BubbleTrouble/src/main/java/Group_24/BubbleTrouble/Room.java
@@ -12,8 +12,10 @@ import java.awt.*;
 public class Room extends JPanel implements ActionListener{
 	
 	private Player player;
+	private Bubble bubble;
 	private Timer timer;
     private final int DELAY = 10;
+    private Color drawingcolor = Color.white;
 	
 	private final int ROOM_WIDTH = 300;
 	private final int ROOM_HEIGHT = 300;
@@ -24,6 +26,7 @@ public class Room extends JPanel implements ActionListener{
 		setFocusable(true);
 		
 		player = new Player();
+		bubble = new Bubble(10, 5, 100, 100);
 		
 		setPreferredSize(new Dimension(ROOM_WIDTH, ROOM_HEIGHT));
 		
@@ -44,14 +47,40 @@ public class Room extends JPanel implements ActionListener{
 		
 		Graphics2D g2d = (Graphics2D) g;
    
-		g2d.setColor(Color.white);
+		g2d.setColor(drawingcolor);
 		g2d.drawRect(player.getX(), ROOM_HEIGHT-player.getHeight(), player.getWidth(), player.getHeight());
+		g2d.setColor(drawingcolor);
+		g2d.drawOval(bubble.getX(), bubble.getY(), bubble.getRealSize(), bubble.getRealSize());
 	}
 	
 	public void actionPerformed(ActionEvent e) {
         
+        // collision detection for bubble against floor
+        if(bubble.getY() + bubble.getRealSize() >= ROOM_HEIGHT){
+        	bubble.collide(Bubble.COLLISIONTYPE_FLOOR);
+        }
+        // collision detection for bubble against left wall
+        if(bubble.getX() <= 0){
+        	bubble.collide(Bubble.COLLISIONTYPE_WALL);
+        }
+        // collision detection for bubble against right wall
+        if(bubble.getX() + bubble.getRealSize() >= ROOM_WIDTH){
+        	bubble.collide(Bubble.COLLISIONTYPE_WALL);
+        }
+        
+        // collision detection for bubble against player
+        double dist_x = Math.abs(bubble.getX() + .5 * bubble.getRealSize() - (player.getX() + .5 * player.getWidth())) - .5 * bubble.getRealSize() - .5 * player.getWidth();
+        double dist_y = Math.abs(bubble.getY() + (.5 * bubble.getRealSize()) - ROOM_HEIGHT + player.getHeight()) - .5 * bubble.getRealSize();
+        
+        if(dist_x <= 0 && dist_y <= 0){
+        	drawingcolor = Color.red;
+        	timer.stop();
+        }
+        
+        // calculate movements and repaint
         player.move();
-        repaint();  
+        bubble.move();
+        repaint();
     }
 	
 	private class TAdapter extends KeyAdapter {
