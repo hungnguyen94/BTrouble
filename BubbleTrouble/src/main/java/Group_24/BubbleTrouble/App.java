344324e8ff80730cd6bfbe0111b0cfabe6875e5c
Chris Berg <c.c.berg@student.tudelft.nl>
2015-09-01 12:52:42 +0200

    basic stuff

--- BubbleTrouble/src/main/java/Group_24/BubbleTrouble/App.java
+++ BubbleTrouble/src/main/java/Group_24/BubbleTrouble/App.java
@@ -1,13 +1,34 @@
 package Group_24.BubbleTrouble;
 
+import java.awt.EventQueue;
+import javax.swing.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends JFrame
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	public App() {
+
+        add(new Board());
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Bubble Trouble");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {                
+                JFrame ex = new App();
+                ex.setVisible(true);                
+            }
+        });
     }
 }
