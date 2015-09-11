7d04c5ca232b1b600b40d440f6680645991e0129
Martin Koster <cmkoster@student.tudelft.nl>
2015-09-11 13:11:11 +0200

    Model Test

--- src/test/java/Group_24/BubbleTrouble/ModelTest.java
+++ src/test/java/Group_24/BubbleTrouble/ModelTest.java
@@ -2,16 +2,25 @@ package Group_24.BubbleTrouble;
 
 import static org.junit.Assert.*;
 
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
 import java.util.ArrayList;
 
 import Group_24.BubbleTrouble.Model;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ModelTest {
 
-	private ArrayList<Room> rooms;
+	@Mock private Room room;
+	@Mock private Player player;
 	
 	@Before
 	public void setUp() {
@@ -26,6 +35,35 @@ public class ModelTest {
 	@Test
 	public void getRoomWidthTest() {
 		assertEquals(Model.getRoomWidth(), 1123);
+	}
+	
+	@Test
+	public void addRoomTest() {
+		Model.addRoom(room);
+		assertEquals(Model.getCurrentRoom(), room);
+	}
+	
+	@Test
+	public void getBubblesTest() {
+		ArrayList<Bubble> bubbles = new ArrayList<Bubble>();
+		Model.addRoom(room);
+		when(room.getBubbles()).thenReturn(bubbles);
+		assertEquals(Model.getBubbles(), bubbles);
+	}
+	
+	@Test
+	public void addPlayerTest() {
+		ArrayList<Player> players = new ArrayList<Player>();
+		players.add(player);
+		Model.addPlayer(player);
+		assertEquals(players, Model.getPlayers());
+	}
+	
+	@Test
+	public void restartRoomTest() {
+		Model.addRoom(room);
+		Model.restartRoom();
+		verify(room).reload();
 	}
 
 
