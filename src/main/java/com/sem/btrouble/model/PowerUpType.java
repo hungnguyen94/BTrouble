5b7d411851e91744eb9e827fb113ff6d805d5a10
Hung Nguyen <H.Nguyen-3@student.tudelft.nl>
2015-10-27 12:34:55 +0100

    Delete obsolete classes.
    < organisation

--- src/main/java/com/sem/btrouble/model/PowerUpType.java
+++
@@ -1,9 +0,0 @@
-package com.sem.btrouble.model;
-
-/**
- * Enumerate for the powerUpTypes.
- */
-public enum PowerUpType {
-    LIFEPOWERUP,
-    ROPEHOLDPOWERUP;
-}
--- src/test/java/com/sem/btrouble/SinglePlayerSurvivalTest.java
+++ src/test/java/com/sem/btrouble/SinglePlayerSurvivalTest.java
@@ -1,64 +1,72 @@
-//package com.sem.btrouble;
-//
-//import com.sem.btrouble.game.AbstractGame;
-//import com.sem.btrouble.game.SinglePlayerSurvivalGame;
-//import com.sem.btrouble.model.Player;
-//import com.sem.btrouble.model.Room;
-//import com.sem.btrouble.observering.Direction;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertTrue;
-//
-///**
-// * This is a end to end test
-// */
-//public class SinglePlayerSurvivalTest {
-//    AbstractGame game;
-//    Player player;
-//
-//    @Before
-//    public void setUp() {
-//        Room room = new Room();
-//        room.loadRoom();
-//        player = new Player(0f, 0f);
-//        game = new SinglePlayerSurvivalGame(room);
-//        game.addPlayer(player);
-//        assertFalse(game.isLevelRunning());
-//        game.startGame();
-//        try {
-//            Thread.sleep(3000);
-//        } catch(InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @Test
-//    public void singlePlayerSurvivalTest() {
-//        game.updateGame();
-//        assertTrue(game.isLevelRunning());
-//        for(int i = 0; i < 10; i++) {
-//            float oldCenterX = player.getCenterX();
-//            game.movePlayer(player, Direction.LEFT, 10);
-//            assertTrue(player.getCenterX() < oldCenterX);
-//            game.updateGame();
-//        }
-//        for(int i = 0; i < 10; i++) {
-//            float oldCenterX = player.getCenterX();
-//            game.movePlayer(player, Direction.RIGHT, 10);
-//            assertTrue(player.getCenterX() > oldCenterX);
-//            game.updateGame();
-//        }
-//        assertTrue(player.canFireRope());
-//        game.fireRope(player);
-//        assertFalse(player.canFireRope());
-//        // Lose the game by getting hit by a bubble
-//        assertTrue(game.isLevelRunning());
-//        while(game.isLevelRunning()) {
-//            game.updateGame();
-//        }
-//        assertFalse(game.isLevelRunning());
-//
-//    }
-//}
+package com.sem.btrouble;
+
+import com.sem.btrouble.game.AbstractGame;
+import com.sem.btrouble.game.SinglePlayerSurvivalGame;
+import com.sem.btrouble.model.Bubble;
+import com.sem.btrouble.model.Player;
+import com.sem.btrouble.model.Room;
+import com.sem.btrouble.observering.Direction;
+import com.sem.btrouble.tools.DataLoader;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * This is a end to end test
+ */
+public class SinglePlayerSurvivalTest {
+    AbstractGame game;
+    Player player;
+
+    @Before
+    public void setUp() {
+        DataLoader dt = new DataLoader(DataLoader.STANDARD_LOCATION);
+        Room room = dt.loadRoom(1);
+        player = new Player(0f, 0f);
+        game = new SinglePlayerSurvivalGame(room);
+        game.addPlayer(player);
+
+        List<Bubble> bubbleList = new ArrayList<>();
+        bubbleList.add(new Bubble(3, 500, 200));
+        game.spawnBubbles(bubbleList);
+
+        assertFalse(game.isLevelRunning());
+        game.startGame();
+        try {
+            Thread.sleep(3000);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void singlePlayerSurvivalTest() {
+        game.updateGame();
+        assertTrue(game.isLevelRunning());
+        for(int i = 0; i < 10; i++) {
+            float oldCenterX = player.getCenterX();
+            game.movePlayer(player, Direction.LEFT, 10);
+            assertTrue(player.getCenterX() < oldCenterX);
+        }
+        for(int i = 0; i < 10; i++) {
+            float oldCenterX = player.getCenterX();
+            game.movePlayer(player, Direction.RIGHT, 10);
+            assertTrue(player.getCenterX() > oldCenterX);
+        }
+        assertTrue(player.canFireRope());
+        game.fireRope(player);
+        assertFalse(player.canFireRope());
+        // Lose the game by getting hit by a bubble
+        assertTrue(game.isLevelRunning());
+        while(game.isLevelRunning()) {
+            game.updateGame();
+        }
+        assertFalse(game.isLevelRunning());
+
+    }
+}
