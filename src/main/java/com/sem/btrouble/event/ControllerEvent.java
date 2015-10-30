509665d3ce7a1b952e9368c0d2537a3ee84b2428
rubenwiersma <r.t.wiersma@student.tudelft.nl>
2015-10-30 17:39:55 +0100

    Deleted unused enums

--- src/main/java/com/sem/btrouble/event/ControllerEvent.java
+++ src/main/java/com/sem/btrouble/event/ControllerEvent.java
@@ -5,5 +5,5 @@ package com.sem.btrouble.event;
  * which can be used to write events to log files.
  */
 public enum ControllerEvent implements Event {
-    GAMEOVER, GAMESTART, NEXTROOM, RESTARTROOM, OUTOFTIME, GAMEWON, GAMELOST;
+    NEXTROOM, RESTARTROOM, GAMEWON, GAMELOST
 }
